30ff3f30ac56cf7cd8f05ef01b9b4856c5c2beb2
HasanC14 <chowdhuryh999@gmail.com>
2024-06-21 15:53:59 +0600

    trying to fix techland

--- server.js
+++ server.js
@@ -86,18 +86,29 @@ const startScraping = async (product) => {
         console.log("No popup found.");
       }
 
-      await page.waitForSelector(".header-search input[name=search]", {
-        visible: true,
-        timeout: 60000,
-      });
-      await page.type(".header-search input[name=search]", product);
+      await page.waitForSelector(
+        ".mobile-search-wrapper .header-search input[name=search]",
+        {
+          visible: true,
+          timeout: 60000,
+        }
+      );
+      await page.type(
+        ".mobile-search-wrapper .header-search input[name=search]",
+        product
+      );
 
-      await page.waitForSelector(".header-search button", {
-        visible: true,
-        timeout: 60000,
-      });
+      await page.waitForSelector(
+        ".mobile-search-wrapper .header-search button",
+        {
+          visible: true,
+          timeout: 60000,
+        }
+      );
       await page.evaluate(() => {
-        document.querySelector(".header-search button").click();
+        document
+          .querySelector(".mobile-search-wrapper .header-search button")
+          .click();
       });
 
       await page.waitForSelector(".main-products-wrapper", {
@@ -334,8 +345,8 @@ const startScraping = async (product) => {
   };
 
   const page = await browser.newPage();
-  await scrapeStarTech(page);
-  // await scrapeTechLand(page);
+  // await scrapeStarTech(page);
+  await scrapeTechLand(page);
   // await scrapeRyans(page);
   // await scrapePcHouse(page);
   // await scrapeUltraTech(page);
